9dcfa62f2170078152e5b08caff8eac4a7ae03de
Krzysztof Mansz <krzysztof.mansz@gmail.com>
2017-05-04 11:58:18 +0200

    construktors in classes

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
 'use strict';
 
-function Animal(voice) {
-  this.voice = voice || 'grunt';
-}
-Animal.prototype.speak = function() {
-  alert(this.voice)
-};
-
-function Cat(name, color) {
-  Animal.call(this, 'Meow');
-  this.name = name;
-  this.color = color;
+class Animal{
+    constuctor(voice){
+      this.voice = voice || 'grunt';
+    }
+    speak(){
+      alert(this.voice);
+    }
 }
 
-Cat.prototype = Object.create(Animal.prototype); //object.create is not going to call the Animal function
-//it is just goint to set that function as the prototype and set up the prototype chain
-var fluffy = new Cat('Fluffy', 'White');
-fluffy.speak();
+class Cat extends Animal{
+    constructor(name, color){
+      super('Meow');
+      this.name = name;
+      this.color = color;
+    }
+}
 
-alert(fluffy instanceof Cat); //it is true correctly
-alert(fluffy instanceof Animal); //it is true also
+var Fluffy = new Cat('Fluffy', 'White');
+Fluffy.speak();
